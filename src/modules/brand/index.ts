a48b70e11625f4a0021031a31cbaf6014c8f4309
Lê Văn Sỷ <levansiht@MacBook-Pro-cua-Le.local>
2025-09-22 00:07:41 +0700

    Add Brand Module & Architecture Improvements

---
+++ src/modules/brand/index.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { Sequelize } from "sequelize";
+import { MySQLBrandRespository } from "./infras/repository/sequelize";
+import { init } from "./infras/repository/sequelize/dto";
+import { CreateNewBrandCmdHandler } from "./usecase/create-new-brand";
+import { GetBrandDetailQuery } from "./usecase/get-brand-detail";
+import { UpdateBrandCmdHandler } from "./usecase/update-brand";
+import { DeleteBrandCmdHandler } from "./usecase/delete-brand";
+import { ListBrandQuery } from "./usecase/list-brand";
+import { BrandHttpService } from "./infras/transport/http-service";
+
+
+export const setupBrandHexagon = (sequelize: Sequelize) => {
+  init(sequelize);
+
+  const repository = new MySQLBrandRespository(sequelize);
+
+  const createCmdHandler = new CreateNewBrandCmdHandler(repository);
+  const getDetailQueryHandler = new GetBrandDetailQuery(repository);
+  const updateCmdHandler = new UpdateBrandCmdHandler(repository);
+  const deleteCmdHandler = new DeleteBrandCmdHandler(repository);
+  const listQueryHandler = new ListBrandQuery(repository);
+
+  const httpService = new BrandHttpService(
+    createCmdHandler,
+    getDetailQueryHandler,
+    updateCmdHandler,
+    deleteCmdHandler,
+    listQueryHandler
+  );
+
+  const router = Router();
+
+  router.post('/brands', httpService.createAPI.bind(httpService));
+  router.get('/brands/:id', httpService.getDetailAPI.bind(httpService));
+  router.get('/brands', httpService.listAPI.bind(httpService));
+  router.patch('/brands/:id', httpService.updateAPI.bind(httpService));
+  router.delete('/brands/:id', httpService.deleteAPI.bind(httpService));
+
+  return router;
+};
---
+++ src/share/interface/index.ts
@@ -0,0 +1,23 @@
+import { PagingDTO } from "../model/paging";
+
+export interface IRepository<Entity, Condition, UpdateDTO> extends IQueryRepository<Entity, Condition>, ICommandRepository<Entity, UpdateDTO> { }
+
+export interface IQueryRepository<Entity, Condition> {
+  get(id: string): Promise<Entity | null>;
+  findByCond(cond: Condition): Promise<Entity | null>;
+  list(cond: Condition, paging: PagingDTO): Promise<Array<Entity>>;
+}
+
+export interface ICommandRepository<Entity, UpdateDTO> {
+  insert(data: Entity): Promise<boolean>;
+  update(id: string, data: UpdateDTO): Promise<boolean>;
+  delete(id: string, isHard: boolean): Promise<boolean>;
+}
+
+export interface ICommandHandler<Command, Result> {
+  execute(command: Command): Promise<Result>;
+}
+
+export interface IQueryHandler<Query, Result> {
+  query(query: Query): Promise<Result>;
+}
